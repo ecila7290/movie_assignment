5a692d23925546a1f143b36a4cd0ac0555f9c17e
ecila7290 <jihoon.kim89@gmail.com>
2021-01-15 21:06:00 +0900

    Edit: edited sample data uploader

--- data_uploader.py
+++ data_uploader.py
@@ -19,7 +19,7 @@ def upload_data():
     Movie.objects.bulk_create([
         Movie(title='School of rock', year=2003, genre_id=1, country='US', stars=3.8, runtime=108, description='Movie school of rock', image_url='rockimage.com'),
         Movie(title='Baby driver', year=2017, genre_id=2, country='US', stars=3.7, runtime=113, description='Movie baby driver', image_url='driverimage.com'),
-        Movie(title='Kings speech', year=2010, genre_id=2, country='UK', stars=3.7, runtime=118, description='Movie kings speech', image_url='speechimage.com')  
+        Movie(title='Kings speech', year=2010, genre_id=3, country='UK', stars=3.7, runtime=118, description='Movie kings speech', image_url='speechimage.com')  
     ])
     print('Uploaded successfully')
 
